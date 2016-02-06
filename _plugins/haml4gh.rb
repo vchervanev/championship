976397f27455dc458f40fc10322d386c7e03319a
Vlad Chervanev <vlad.chervanev@castle.co>
2016-02-06 09:51:46 +0400

    Plugin: break regeneration cycle

--- _plugins/haml4gh.rb
+++ _plugins/haml4gh.rb
@@ -6,20 +6,37 @@ module GithubPagesHamlHelper
   class Generator < Jekyll::Generator
     def generate(site)
 
-      plugin_config = site.config["haml4gh"]
-      source = File.join(site.config["source"], (plugin_config && plugin_config["source"] || "_source"))
-
+      source = parse_source(site)
       destination = site.config["source"]
 
       Dir.glob(File.join(source, '*.haml')).each do |source_file|
-        destination_file = source_file.sub(source, destination).sub(/\.haml$/, '.html')
+        destination_file = source_file.sub(source, destination).sub(/\.haml$/i, '.html')
+
+        if older?(destination_file, source_file)
+          log 'skipped ' + destination_file
+          next
+        end
+
         # this will stop current process
         # opts = Haml::Exec::Haml.new([source_file, destination_file])
         # opts.parse!
-        p `haml #{source_file} #{destination_file}`
-        p "#{source_file} -->> #{destination_file}"
+        log `haml #{source_file} #{destination_file}`
+        log "#{source_file} -->> #{destination_file}"
       end
 
+    end
+
+    def parse_source(site)
+      plugin_config = site.config["haml4gh"]
+      File.join(site.config["source"], (plugin_config && plugin_config["source"] || "_source"))
+    end
+
+    def older?(destination_file, source_file)
+      File.exists?(destination_file) && (File.mtime(destination_file) >= File.mtime(source_file))
+    end
+
+    def log(msg)
+      print "\nhaml4gh: #{msg}" unless msg.empty?
     end
   end
 end
