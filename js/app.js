3d6dbd6eed293a58eebb6df5e732cb47390bc2dc
Vlad Chervanev <vlad.chervanev@castle.co>
2016-02-22 17:08:11 +0400

    Added smart highlighting on mouse-over event

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
-(function(angular) {
+(function (angular) {
 
-    var myApp = angular.module('Championship', []);
+  var myApp = angular.module('Championship', []);
 
-    myApp.controller('ChampionshipCtrl', ['$scope', 'INPUT', function ($scope, input) {
-        $scope.input = input;
+  myApp.controller('ChampionshipCtrl', ['$scope', 'INPUT', function ($scope, input) {
+    $scope.input = input;
 
-        $scope.cellStyle = function (playerA, playerB){
-            if (playerA == playerB) {
-                return 'dark';
-            }
-        }
-    }]);
+    $scope.cellStyle = function(rowPlayer, colPlayer) {
+      if (rowPlayer == colPlayer) {
+        return 'dark';
+      } else if ($scope.focusedRowPlayer && ($scope.focusedRowPlayer == rowPlayer || $scope.focusedColPlayer == colPlayer)){
+        return 'highlight';
+      } else if ($scope.focusedRowPlayer == colPlayer && $scope.focusedColPlayer == rowPlayer){
+        return 'vs-highlight';
+      }
+    };
+    $scope.mouseOver = function(rowPlayer, colPlayer) {
+      $scope.focusedRowPlayer = rowPlayer;
+      $scope.focusedColPlayer = colPlayer;
+    };
+  }]);
 
 })(window.angular);
